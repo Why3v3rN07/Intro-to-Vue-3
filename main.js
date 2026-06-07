ce0e8b7d7ab172c6d675250c42abd53373b157d8
Why3v3rN07 <hfogarty@student.touro.edu>
2026-06-07 17:26:17 -0400

    L5

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            ],
+            sizes: [
+                { id: 1, name: 'Small' },
+                { id: 2, name: 'Medium' },
+                { id: 3, name: 'Large' }
+            ]
         }
     }
 })
